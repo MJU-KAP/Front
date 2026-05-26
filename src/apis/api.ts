0843d8c453c4f5092ed7871d8163147f5fd4b064
ZZGG2 <tkdxm0208@gmail.com>
2026-05-26 19:27:12 +0900

    에러 수정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -10,7 +10,6 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
   timeout: 180000,
   withCredentials: true, 
-  headers: { 'Content-Type': 'application/json' },
 });
 
 // 요청 인터셉터
